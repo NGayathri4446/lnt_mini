964693955a6f56d946840e27a9af28212277cfd1
NGayathri4446 <58762705+NGayathri4446@users.noreply.github.com>
2021-04-16 15:32:57 +0530

    Create append.c

---
+++ 3_implementation/src/append.c
@@ -0,0 +1,20 @@
+#include<header.c>
+int append() {
+    FILE *fp;
+    char note[2000], date[20] ;
+    welcome_message();
+    printf("Enter date of your note : ");
+    scanf("%s", date);
+    //printf("%s",date);
+    printf("\n");
+    printf("Just write your note from here....\n---->>");
+    scanf(" %[^\n]s", note);
+    fp = fopen("diary.txt", "a");
+    fprintf(fp, date);
+    fprintf(fp, "\n");
+    fprintf(fp, note);
+    fprintf(fp, "\n\n");
+    printf("\nSuccessfully Copied.....:)\n");
+    fclose(fp);
+    return 0;
+}
