861093c28bcd505a463bca06130cf038ff43c3fe
NGayathri4446 <58762705+NGayathri4446@users.noreply.github.com>
2021-04-16 15:30:04 +0530

    Create welcome_message.c

---
+++ 3_implementation/src/welcome_message.c
@@ -0,0 +1,6 @@
+#include<header.c>
+void welcome_message() {
+    printf("---***---***----***----***----***---\n\n");
+    printf("|<--     Welcome to our Diary    -->|\n\n");
+    printf("---***---***----***----***---***---\n\n");
+}
